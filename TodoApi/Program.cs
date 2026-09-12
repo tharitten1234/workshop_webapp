be9060fb0d99c6e7a9a0fee82f47349d215cf042
tharitten1234 <taritten@gmail.com>
2026-09-12 17:53:53 +0700

    [dev] add POST endpoint for todos and create TodoPostDto

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -20,9 +20,26 @@ var todos = new List<TodoGetDto>
 {
     new(1, "Learn C#", true),
     new(2, "Learn ASP.NET Core", false),
-    new(3, "Build a web API", false)    
+    new(3, "Build a web API", false)
 };
 
 app.MapGet("/api/todos",()=> Results.Ok(todos));
-    
+
+app.MapGet("/api/todos/{id}", (int id) =>
+{
+    var todo = todos.FirstOrDefault(x => x.Id == id);
+
+    return todo;
+
+});
+
+app.MapPost("/api/todos", (TodoPostDto dto) =>
+{
+    var nextId = todos.Count == 0 ? 1 : todos.Max(x => x.Id) + 1;
+    var todo = new TodoGetDto(nextId, dto.Title, false);
+    todos.Add(todo);
+
+    return Results.Created($"/api/todos/{todo.Id}", todo);
+});
+
 app.Run();
